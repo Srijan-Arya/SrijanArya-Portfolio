842a94ac3b8f8fd89c3bcf5f29dde640469609bc
Srijan Arya <74716986+Srijan-Arya@users.noreply.github.com>
2024-09-16 01:54:45 +0530

    loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
-import './app.scss'
-import './intro.scss'
-import Navbar from "./components/navbar/Navbar"
-import Intro from "./Intro"
-import About from './About'
-import Project from './Project'
-import Contact from './Contact'
-// import ProfileEmbed from './ProfileEmbed';
+import './app.scss';
+import './intro.scss';
+import Navbar from "./components/navbar/Navbar";
+import Intro from "./Intro";
+import About from './About';
+import Project from './Project';
+import Contact from './Contact';
+import React, { useState, useEffect } from 'react';
+import { PacmanLoader } from 'react-spinners';
+
 const App = () => {
-  return <div>
-    <section className='landing_section'>
-      <Navbar />
-      <Intro />
-    </section>
-    <section id="about" className='about_section'><About/></section>
-    <section id="project"><Project /></section>
-    <section id="connect"><Contact /></section>
-  </div>;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading time of 3 seconds
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <div className="spinner-container">
+          <PacmanLoader color="gray" />
+        </div>
+      ) : (
+        <>
+          <section className='landing_section'>
+            <Navbar />
+            <Intro />
+          </section>
+          <section id="about" className='about_section'>
+            <About />
+          </section>
+          <section id="project">
+            <Project />
+          </section>
+          <section id="connect">
+            <Contact />
+          </section>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
